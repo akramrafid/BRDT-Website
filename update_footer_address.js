002763545385b6f1a33ed9d8e9cd5abcae92a1a5
BRDT Website <akramrafid@users.noreply.github.com>
2026-06-03 13:59:25 +0600

    feat: add frontend pages and implement zakat calculator functionality

--- update_footer_address.js
+++ update_footer_address.js
@@ -3,33 +3,24 @@ const path = require('path');
 
 const directoryPath = path.join(__dirname, 'frontend', 'public');
 
-const replacement = `<div class="footer-col address-col">
-        <h4>BRDT</h4>
-        <p>
-          Village: Belghar; Union: Adra North<br />
-          Upazilla: Nangalkot; Cumilla, Bangladesh
-        </p>
-        <p style="margin-top: 15px;">
-          Mobile: +44 (0)7540 253384<br />
-          Email: brdtbd@gmail.com<br />
-          Web: www.brdtrust.com
-        </p>
-      </div>`;
-
 function processFile(filePath) {
     let content = fs.readFileSync(filePath, 'utf8');
 
-    // Replace the address column content
-    const regex = /<div class="footer-col address-col">[\s\S]*?<\/div>\s*<\/div>\s*<\/footer>/;
+    // Replace the specific text block
+    const oldText = 'Village: Belghar; Union: Adra North<br />\n          Upazilla: Nangalkot; Cumilla, Bangladesh';
+    const oldText2 = 'Village: Belghar; Union: Adra North<br />\r\n          Upazilla: Nangalkot; Cumilla, Bangladesh';
+    const oldText3 = 'Village: Belghar; Union: Adra North<br/>\n          Upazilla: Nangalkot; Cumilla, Bangladesh';
     
+    // Using Regex to be robust against whitespace differences
+    const regex = /Village: Belghar;\s*Union:\s*Adra\s*North\s*<br\s*\/>\s*Upazilla:\s*Nangalkot;\s*Cumilla,\s*Bangladesh/;
+    const newText = 'Village: Belghar; Union: Adra North, Upazilla: Nangalkot; Cumilla, Bangladesh';
+
     if (regex.test(content)) {
-        const newContent = content.replace(regex, `${replacement}\n    </div>\n  </footer>`);
+        const newContent = content.replace(regex, newText);
         if (newContent !== content) {
             fs.writeFileSync(filePath, newContent, 'utf8');
             console.log(`Updated: ${filePath}`);
         }
-    } else {
-        console.log(`Pattern not found in: ${filePath}`);
     }
 }
 
